91fbf91bad85f9c9157e8074c2fd6af58951739c
Akila <akila@Akilas-MacBook-Air.local>
2024-11-16 17:56:30 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Box from '@mui/material/Box';
+import SearchIcon from '@mui/icons-material/Search';
+
+import useStyles from './styles.js';
+
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
